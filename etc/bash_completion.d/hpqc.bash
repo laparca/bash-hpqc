1c355ef34efc103d7b8672933e7af4254acb0ebc
srs003es <samuel.rodriguez@diagroup.com>
2013-10-09 13:53:47 +0200

    Fixing completion

--- etc/bash_completion.d/hpqc.bash
+++ etc/bash_completion.d/hpqc.bash
@@ -1,12 +1,13 @@
 _hpqc() {
+    local cur prev opts
     COMPREPLY=()
-	PLUGINGS="$(hpqc list-plugins)"
+	opts="$(hpqc list-plugins)"
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-	COMPREPLY=( $(compgen -W "${PLUGINS}" -- "${cur}") )
+	COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
 	return 0
 }
-complete -o filenames -F _hpqc hpqc
+complete -F _hpqc hpqc
 
 # vim: set ft=sh tw=80 sw=4 et :
